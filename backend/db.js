1dc0d0c1f298c7cfcd72896aee8ba1387d7ee0d3
Jun Kai Zhang <junkaizhang8@gmail.com>
2024-07-28 23:00:02 -0400

    Change postgres container port to avoid conflict with the device's postgres port

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 import pg from "pg";
 
 export const pool = new pg.Pool({
-  host: "db",
-  port: 5432,
-  user: "postgres",
-  password: "a",
-  database: "c43pgdb",
+  host: process.env.DB_HOST || "localhost",
+  port: 5430,
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "a",
+  database: process.env.DB_NAME || "c43pgdb",
 });
