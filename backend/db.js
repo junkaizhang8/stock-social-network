7d8c2f223bddf8d75d50d4e7db52ce9a18ec52ba
Jun Kai Zhang <junkaizhang8@gmail.com>
2024-07-30 23:16:28 -0400

    Fix server container connection error by connecting to the correct port

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import pg from "pg";
 
 export const pool = new pg.Pool({
   host: process.env.DB_HOST || "localhost",
-  port: 5430,
+  port: process.env.DB_PORT || 5430,
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "a",
   database: process.env.DB_NAME || "c43pgdb",
