fafa1089e915d25c275d523eb3ee953684b67712
Jyarn <58491036+Jyarn@users.noreply.github.com>
2024-08-01 13:17:59 -0400

    push basic home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,47 @@
 import { useState } from 'react';
 import Login from './pages/login/Login.jsx';
+import Portfolio from './pages/portfolio/Portfolio.jsx'
 import './App.css'
 
 function App() {
+  const [page_num, setPageNum] = useState(0); 
+  let page;
+  if (page_num == 0)
+    return <Login onVerification={() => setPageNum(1)}/>
+  else if (page_num == 1)
+    page = <Portfolio/>
+  else if (page_num == 2)
+    page = <p>:D</p>
+
+  const logout = () => {
+    console.log("logout ):");
+    setPageNum(0);
+  }
+
   return (
     <>
-      <Login/>
+      <div className="page-selector">
+        <button 
+          onClick={() => setPageNum(1)} 
+          disabled={page_num == 1}
+          className="page-button">
+            Portfolios 
+        </button>
+        <button 
+          onClick={() => setPageNum(2)} 
+          disabled={page_num == 2}
+          className="page-button">
+            Profile
+        </button>
+        <button 
+          onClick={logout}
+          className="page-button">
+            Sign Out
+        </button>
+      </div>
+      <div>
+       {page}
+      </div>
     </>
   )
 }
