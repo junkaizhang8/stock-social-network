94e28b6b781b41571c51a62fb70bcf6ebc10cfbd
Jyarn <58491036+Jyarn@users.noreply.github.com>
2024-08-01 11:32:30 -0400

    error handling in login page

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,26 +2,39 @@ import { useState, useEffect } from 'react';
 import apiService from '../../services/api';
 import './Login.css';
 
-const Login = () => {
+const Login = ({ onVerification }) => {
   const [msg, setMsg] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const login = async (e) => {
-    e.preventDefault();
-    apiService.signIn(username, password).then((res) => {
-      console.log(res);
-    });
+  const handleVerification = (res) => {
+    res.then((res) => {
+      const body = res.data;
+      if (res.status != 200)
+        setMsg(body.error);
+      else {
+        setMsg(body.message);
+        onVerification();
+      }
+    }).catch((e) => {
+      setMsg(e.response.data.error);
+    })
   }
 
-  const signup = async (e) => {
+  const login = (e) => {
     e.preventDefault();
-    apiService.signUp(username, password).then((res) => {
-      console.log(res);
-    });
+    if (username != "" && password != "")
+      handleVerification(apiService.signIn(username, password));
+  }
+
+  const signup = (e) => {
+    e.preventDefault();
+    if (username != "" && password != "")
+      handleVerification(apiService.signUp(username, password));
   }
 
   const toggleForm = () => {
+    setMsg("");
     const forms = document.querySelectorAll('.complex-form');
     forms.forEach(form => form.classList.toggle('hidden'));
   }
@@ -36,6 +49,8 @@ const Login = () => {
         type="text"
         name="username"
         placeholder="Username"
+        onChange={f => setUsername(f.target.value)}
+        value={username}
         required
       />
       <input
@@ -43,10 +58,13 @@ const Login = () => {
         type="password"
         name="password"
         placeholder="Password"
+        onChange={f => setPassword(f.target.value)}
+        value={password}
         required
       />
       <a className="new-user-select" onClick={toggleForm}>New User?</a>
       <input className="form-submit" type="submit" value="Sign In" />
+      {msg == "" ? <></> : <p>{msg}</p>}
     </form>
     <form className="complex-form sign-in-form hidden" onSubmit={signup}>
       <div className="form-title">Sign Up</div>
@@ -55,6 +73,8 @@ const Login = () => {
         type="text"
         name="username"
         placeholder="Username"
+        onChange={f => setUsername(f.target.value)}
+        value={username}
         required
       />
       <input
@@ -62,6 +82,8 @@ const Login = () => {
         type="password"
         name="password"
         placeholder="Password"
+        onChange={f => setPassword(f.target.value)}
+        value={password}
         required
       />
       <a className="new-user-select" onClick={toggleForm}>Already a User?</a>
